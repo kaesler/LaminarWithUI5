0bd7dab2e1404fecda233f06bd8c56425aebbc6b
Kevin Esler <kevin.esler@gmail.com>
2023-07-18 14:26:23 -0400

    Place header bar right at the top

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,7 +1,12 @@
 package example
 
-import be.doeraene.webcomponents.ui5.Input
-import be.doeraene.webcomponents.ui5.configkeys.ValueState
+import be.doeraene.webcomponents.ui5.{Bar, Button, Input, Label}
+import be.doeraene.webcomponents.ui5.configkeys.{
+  BarDesign,
+  ButtonDesign,
+  IconName,
+  ValueState
+}
 import com.raquo.laminar.api.L.{*, given}
 import org.scalajs.dom
 
@@ -9,16 +14,42 @@ import org.scalajs.dom
 def helloWorld(): Unit =
   renderOnDomContentLoaded(
     dom.document.getElementById("app"),
-    // ButtonAppElement.appElement()
     myDiv
   )
 
 def myDiv =
   div(
-    Input(
-      _.required    := true,
-      _.valueState  := ValueState.Information,
-      _.placeholder := "Enter your name",
-      onChange.mapToValue --> Observer(println)
+    myHeaderBar,
+    anInput,
+    myFooterBar
+  )
+
+def myHeaderBar =
+  Bar(
+    _.design := BarDesign.Header,
+    _.slots.startContent := Button(
+      _.icon    := IconName.home,
+      _.tooltip := "Go home",
+      _.design  := ButtonDesign.Transparent
+    ),
+    title := "TipsLadder.com",
+    _ => Label("TipsLadder.com"),
+    _.slots.endContent := Button(
+      _.icon    := IconName.document,
+      _.tooltip := "Doc",
+      _.design  := ButtonDesign.Transparent
     )
+  )
+
+def anInput =
+  Input(
+    _.required    := true,
+    _.valueState  := ValueState.Information,
+    _.placeholder := "Enter your name",
+    onChange.mapToValue --> Observer(println)
+  )
+
+def myFooterBar =
+  Bar(
+    _.design := BarDesign.Footer
   )
